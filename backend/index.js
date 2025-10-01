153bd084a26684e905105cc5b922514e5e3ec097
yami <ravirrajpatil@gmail.com>
2025-10-02 00:59:14 +0530

    refactor: Clean up import statements and improve code formatting in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,25 @@
 import express from 'express';
-import {nanoid} from "nanoid"
+import { nanoid } from "nanoid";
 import dotenv from 'dotenv';
 import cors from 'cors';
-import  {connectToMongoDB}  from "./src/config/mongo.config.js"
-dotenv.config("./.env");
+import { connectToMongoDB } from "./src/config/mongo.config.js";
 import cookieParser from 'cookie-parser';
 import { errorHandler } from './src/middleware/error.middleware.js';
 
+import shortUrlRoutes from "./src/routes/short_url.routes.js";
+import userRoutes from "./src/routes/user.routes.js";
 
-import shortUrlRoutes from "./src/routes/short_url.routes.js"
-import userRoutes from "./src/routes/user.routes.js"
-
+dotenv.config();
 
 const app = express();
+
 app.use(cors({
     origin: process.env.BASE_URL_FRONTEND,
     credentials: true, // Allow cookies to be sent
 }));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
-
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.use("/api/url", shortUrlRoutes);
@@ -28,12 +27,18 @@ app.use("/api/auth", userRoutes);
 
 app.use(errorHandler);
 
-
-app.get("/",(req,res)=>{
-    res.send("Welcome to URL Shortener API");
-})
-
-app.listen(process.env.PORT,()=>{
-    connectToMongoDB();
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.get("/", (req, res) => {
+    res.send("Welcome to URL Shortener API 🚀");
 });
+
+
+if (process.env.NODE_ENV !== "production") {
+    app.listen(process.env.PORT || 3000, () => {
+        connectToMongoDB();
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+} else {
+    connectToMongoDB();
+}
+
+export default app;
